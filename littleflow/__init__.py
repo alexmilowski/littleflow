4d2f522e2dea6ba27dec0d63f875e29d29104b8e
Alex Milowski <alex@microbyre.com>
2022-06-25 11:57:49 -0700

    added version

--- littleflow/__init__.py
+++ littleflow/__init__.py
@@ -1,3 +1,4 @@
+__version__=(0,3,0)
 from .parser import Parser
 from .model import Workflow, SubFlow, Statement, Start, End, Iterate, Task, LiteralSource, ResourceSource, ResourceSink, ParameterLiteral
 from .compiler import Compiler
