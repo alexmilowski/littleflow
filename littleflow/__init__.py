bb1e7b5e4252b0f25d0851ca086775e74feef765
Alex Milowski <alex@microbyre.com>
2022-06-24 18:06:17 -0700

    added runner

--- littleflow/__init__.py
+++ littleflow/__init__.py
@@ -1,3 +1,4 @@
 from .parser import FlowParser
 from .model import Workflow, SubFlow, Statement, Start, End, Iterate, Task, LiteralSource, ResourceSource, ResourceSink, ParameterLiteral
 from .compiler import FlowCompiler
+from .runner import Context, Runner
