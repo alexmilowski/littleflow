2b80b7ad70257d274016277a6f02d3df1cfaf204
Alex Milowski <alex@microbyre.com>
2022-07-06 22:28:03 -0700

    added flow imports; updated version

--- littleflow/__init__.py
+++ littleflow/__init__.py
@@ -1,9 +1,10 @@
-__version__=(0,6,0)
+__version__=(0,7,0)
 __author__='Alex Miłowski'
 __author_email__='alex@milowski.com'
 from .parser import Parser
 from .model import Workflow, Declaration, SubFlow, Statement, Start, End, Iterate, Task, LiteralSource, ResourceSource, ResourceSink, ParameterLiteral
 from .compiler import Compiler
 from .runner import Context, Runner, InputCache, MemoryInputCache, TaskContext, FunctionTaskContext, pass_input, pass_parameters
+from .flow import Invocation, Source, Sink, InvokeTask, InvokeFlow, Flow
 from .doc import graph_name, graph
 from .utils import run_workflow
