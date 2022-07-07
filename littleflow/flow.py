71d5bb1cb0b3044a3291f5e0616b6453725b2111
Alex Milowski <alex@microbyre.com>
2022-07-06 22:29:27 -0700

    added representation loading and saving

--- littleflow/flow.py
+++ littleflow/flow.py
@@ -1,3 +1,5 @@
+import json
+
 import numpy as np
 from dataclasses import dataclass, field
 
@@ -23,11 +25,24 @@ class InvokeTask(Invocation):
 class InvokeFlow(Invocation):
    pass
 
+
 class Flow:
 
-   def __init__(self,size : int):
-      self._F = np.zeros((size,size),dtype=int)
-      self._tasks = [None]*size
+   __classes = {
+      'Source' : Source,
+      'Sink' : Sink,
+      'InvokeTask' : InvokeTask,
+      'InvokeFlow' : InvokeFlow
+   }
+
+   def __init__(self,size=0,serialized=None):
+      if serialized is None:
+         assert size>0
+         self._F = np.zeros((size,size),dtype=int)
+         self._tasks = [None]*size
+      else:
+         self._F = np.array(serialized[0])
+         self._tasks = [Flow.__classes[T[0]](**T[1]) for T in serialized[1]]
 
    @property
    def F(self):
@@ -41,3 +56,13 @@ class Flow:
 
    def __len__(self):
       return len(self._tasks)
+
+   def __str__(self):
+      return json.dumps(self.save())
+
+   def save(self):
+      R = [
+         self._F.tolist(),
+         [[T.__class__.__name__,T.__dict__] for T in self._tasks]
+      ]
+      return R
