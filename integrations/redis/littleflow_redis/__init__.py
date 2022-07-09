20383a51a53c64e30b8b189152f89be1a2cedf55
Alex Milowski <alex@microbyre.com>
2022-07-09 12:29:31 -0700

    project install setup

--- integrations/redis/littleflow_redis/__init__.py
+++ integrations/redis/littleflow_redis/__init__.py
@@ -1,4 +1,6 @@
-__version__=(0,2,0)
+__version__=(0,3,0)
+__author__='Alex Miłowski'
+__author_email__='alex@milowski.com'
 
 from .context import RedisInputCache
 from .remote import RemoteTaskContext, RedisContext, TaskEndListener, TaskStartListener, LifecycleListener, run_workflow, compute_vector, trace_vector, workflow_archive, restore_workflow, save_workflow, load_workflow, load_workflow_state, workflow_state, is_running, delete_workflow, terminate_workflow, restart_workflow
---
+++ integrations/redis/setup.cfg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+VERSION=`python -c "import littleflow_redis; print('.'.join(map(str,littleflow_redis.__version__)))"`
+AUTHOR=`python -c "import littleflow_redis; print(littleflow_redis.__author__)"`
+EMAIL=`python -c "import littleflow_redis; print(littleflow_redis.__author_email__)"`
+DESCRIPTION="An integration of littleflow with Redis for remote execution"
+REQUIRES=`python -c "list(map(print,['\t'+line.strip() for line in ['littleflow']+open('requirements.txt', 'r').readlines()]))"`
+cat <<EOF > setup.cfg
+[metadata]
+name = littleflow
+version = ${VERSION}
+author = ${AUTHOR}
+author_email = ${EMAIL}
+description = ${DESCRIPTION}
+license = Apache License 2.0
+url = https://github.com/alexmilowski/littleflow
+
+[options]
+packages =
+   littleflow_redis
+include_package_data = True
+install_requires =
+${REQUIRES}
+
+[options.package_data]
+* = *.json, *.yaml
+
+EOF
