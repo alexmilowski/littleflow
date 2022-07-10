cb9231cbbde8725e5876296e8ddd2152b3980459
Alex Milowski <alex@microbyre.com>
2022-07-09 23:28:45 -0700

    added failure functions

--- integrations/redis/littleflow_redis/__init__.py
+++ integrations/redis/littleflow_redis/__init__.py
@@ -3,4 +3,6 @@ __author__='Alex Miłowski'
 __author_email__='alex@milowski.com'
 
 from .context import RedisInputCache
-from .remote import RemoteTaskContext, RedisContext, TaskEndListener, TaskStartListener, LifecycleListener, run_workflow, compute_vector, trace_vector, workflow_archive, restore_workflow, save_workflow, load_workflow, load_workflow_state, workflow_state, is_running, delete_workflow, terminate_workflow, restart_workflow
+from .remote import RemoteTaskContext, RedisContext, TaskEndListener, TaskStartListener, LifecycleListener, run_workflow, compute_vector, trace_vector, \
+                    workflow_archive, restore_workflow, save_workflow, load_workflow, load_workflow_state, workflow_state, is_running, delete_workflow, \
+                    terminate_workflow, restart_workflow, get_failures, set_failures
