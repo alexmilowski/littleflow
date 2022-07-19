b2af03c2c3e47707d41dfb69a90144a60ba14ba7
Alex Milowski <alex@microbyre.com>
2022-07-18 23:22:48 -0700

    added cli main for shiv archive for console

--- integrations/redis/littleflow_redis/console/__main__.py
+++ integrations/redis/littleflow_redis/console/__main__.py
@@ -1,18 +1,4 @@
-import sys
-
-from .service import service
-
-def main():
-   if len(sys.argv)>1:
-      host, sep, port = sys.argv[1].partition(':')
-      if len(sep)==0:
-         port = 8000
-      else:
-         port = int(port)
-   else:
-      host = '0.0.0.0'
-      port = 8000
-   service.run(host=host,port=port)
+from .cli import main
 
 if __name__=='__main__':
    main()
