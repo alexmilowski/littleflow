cfdf383ce2aa1be324bf2304fd0f78bdf4184394
Alex Milowski <alex@microbyre.com>
2022-07-21 22:08:23 -0700

    fix for copying input to output

--- integrations/k8s/littleflow_k8s/batch.py
+++ integrations/k8s/littleflow_k8s/batch.py
@@ -1,6 +1,9 @@
 from rqse import EventListener, message, receipt_for
 from time import sleep
 
+# TODO: should we have this dependency?
+from littleflow_redis import RedisOutputCache
+
 class BatchStart(EventListener):
 
    def __init__(self,key,group,host='0.0.0.0',port=6379,username=None,password=None,pool=None):
@@ -10,7 +13,6 @@ class BatchStart(EventListener):
       workflow_id = event.get('workflow')
       name = event.get('name')
       ns, _, job_name = name.partition(':')
-      print(ns,job_name)
       if ns!='batch':
          return False
       index = event.get('index')
@@ -22,6 +24,12 @@ class BatchStart(EventListener):
 
       # hack for now
       sleep(2)
+
+      output = input
+      if output is not None:
+         cache = RedisOutputCache(self.connection,workflow_id)
+         cache[index] = output
+
       event = {'name':name,'index':index,'workflow':workflow_id}
       event['status'] = 'SUCCESS'
       self.append(message(event,kind='end-task'))
