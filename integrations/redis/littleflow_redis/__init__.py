8466fd07a66935851c8b9b06439398e085ab0c4a
Alex Milowski <alex@microbyre.com>
2022-09-20 00:05:18 -0700

    updated version; added import for request listener

--- integrations/redis/littleflow_redis/__init__.py
+++ integrations/redis/littleflow_redis/__init__.py
@@ -1,7 +1,7 @@
 '''
 An integration of littleflow an Redis for remote execution.
 '''
-__version__=(0,4,0)
+__version__=(0,5,0)
 __author__='Alex Miłowski'
 __author_email__='alex@milowski.com'
 
@@ -10,4 +10,6 @@ from .remote import RemoteTaskContext, RedisContext, TaskEndListener, TaskStartL
                     workflow_archive, restore_workflow, save_workflow, load_workflow, load_workflow_state, workflow_state, is_running, delete_workflow, \
                     terminate_workflow, restart_workflow, get_failures, set_failures
 from .wait import WaitTaskListener
+from .request import RequestTaskListener
+from .auth import create_jwt_credential_actor
 from .cli import cli as main
