6da860a4d16a97615924db59aa447cf58e3f1953
Alex Milowski <alex@microbyre.com>
2023-11-17 10:37:59 -0800

    added exceptions

--- littleflow/utils.py
+++ littleflow/utils.py
@@ -1,5 +1,16 @@
 from littleflow import Parser, Compiler, Runner, Context, FunctionTaskContext
 
+class WorkflowFailure(Exception):
+   pass
+
+class TaskFailure(WorkflowFailure):
+   def __init__(self, message, *args, task=None, **kwargs):
+      if task is None:
+         super().__init__(message,*args,**kwargs)
+      else:
+         super().__init__(f'Task {task} failed: {message}',*args,**kwargs)
+
+
 def run_workflow(workflow,lookup=None,context=None,input=None):
    p = Parser()
    c = Compiler()
@@ -12,9 +23,15 @@ def run_workflow(workflow,lookup=None,context=None,input=None):
    elif lookup is not None:
       context.task_context = FunctionTaskContext(lookup)
 
-   runner.start(context,input=input)
+   try:
+      runner.start(context,input=input)
 
-   while not context.ending.empty():
-      runner.next(context,context.ending.get())
+      while not context.ending.empty():
+         runner.next(context,context.ending.get())
+   except TaskFailure as ex:
+      raise WorkflowFailure('The workflow failed due to a task failure.') from ex
+   except Exception as ex:
+      raise WorkflowFailure('The workflow failed due to an exception.') from ex
+
 
    return context.input_for(len(flow)-1), context
