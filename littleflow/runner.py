fixed bug in threshold calculation; the minimum should be 1

littleflow/runner.py
@@ -99,7 +99,7 @@ class Context:
       self._flow = flow
       self._A = activation if activation is not None else np.zeros((self.F.shape[0],1),dtype=int)
       self._T = flow.F.sum(axis=0)
-      self._T[0] = 1
+      self._T[np.where(self._T == 0)] = 1
       self._T = self._T.reshape((self.F.shape[0],1))
       self._S = state if state is not None else np.zeros((self.F.shape[0],1),dtype=int)
       self._ends = SimpleQueue()